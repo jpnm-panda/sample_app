d1cd450fd4ae3a0431f217d5c45ca9cd02d8dac9
jpnm-panda <jpnm0415shkm@gmail.com>
2021-01-31 00:41:30 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -9,5 +10,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup' , to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
